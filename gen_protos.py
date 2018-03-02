ad827edeb24df205fa5285262d4f7b2ac5883f87
khlam <4841220+khlam@users.noreply.github.com>
2018-03-02 13:19:03 -0800

    Script correctly generates js and python protos. Checking file age is next.

--- gen_protos.py
+++ gen_protos.py
@@ -1,46 +1,45 @@
 #!/usr/bin/env python3
 import os
-from sys import platform
-from subprocess import call
 
-def get_platform():
-    if platform == "linux" or platform == "linux2":
-        return "linux"
-
-    elif platform == "darwin":
-        return "macos"
-
-    elif platform == "win32":
-        return "windows"
-
-def check_age(proto):
-    age = os.stat(proto).st_mtime
-    print(age)
-
-def usage():
-    print("\t\tUsage:")
-    print("\tproto to python \t\t -p")
-    print("\tproto to javascript \t-j")
-
-def gen_viz_protos():
-    path = '../common_protos/'
-    if(os.path.isdir(path)):
+def gen_protos(input_path, output_path, target, lang):
+    if(os.path.isdir(input_path)):
         all_proto = ''
-        for proto in os.listdir(path):
-            all_proto += ' '+ path + proto
+        if target != '':
+            for proto in os.listdir(input_path):
+                all_proto += ' '+ input_path + proto
+            cmd = 'protoc --proto_path='+ input_path +' --'+lang+'_out=library='+target+':'+output_path+ " "+ all_proto
+            if (os.system(cmd) != 0):
+                print('ERROR: '+ target + ' proto generation failed')
+            else:
+                print(cmd)
+                print('Success ' + target + '.' + lang)
+        else:
+            for proto in os.listdir(input_path):
+                cmd = 'protoc --proto_path='+ input_path +' --'+lang+'_out=library:' + output_path +' '+input_path+proto
+                if (os.system(cmd) != 0):
+                    print(cmd)
+                    print('ERROR: '+ proto + ' generation failed')
+                else:
+                    print(cmd)
+                    print('Success ' + proto)
 
-        cmd = 'protoc --proto_path='+ path +' --js_out=library=vizProtos:.' + all_proto
-        os.system(cmd)
+# Generate vizProtos.js for SCAII
+input_path = '../common_protos/'
+output_path = '../viz/js/'
+target = 'vizProtos'
+lang = 'js'
+gen_protos(input_path, output_path, target, lang)
 
-def gen_python_protos():
-    path = '../game_wrapper/'
-    if(os.path.isdir(path)):
-        all_proto = ''
-        for proto in os.listdir(path):
-            all_proto += ' ' + path + proto
-        cmd = 'protoc --proto_path='+ path +' --python_out=library=vizProtos:.' + all_proto
-        print(cmd)
+# Generate all python protos for SCAII
+input_path = '../common_protos/'
+output_path = '../glue/python/scaii/protos/'
+target = ''
+lang = 'python'
+gen_protos(input_path, output_path, target, lang)
 
-
-gen_viz_protos()
-gen_python_protos()
+# Generate all python protos for Sky-rts
+input_path = '../protos/'
+output_path = '../game_wrapper/python/protos/'
+target = ''
+lang = 'python'
+gen_protos(input_path, output_path, target, lang)
