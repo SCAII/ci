2c96b73483b7dd59ae8a57c3449237b58c8b962c
khlam <4841220+khlam@users.noreply.github.com>
2018-03-05 17:44:45 -0800

    Script can be envoked regardless of current working directory, script will now derive correct path to protos and target directory

--- gen_protos.py
+++ gen_protos.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-import os
+import os, fnmatch
 
 def gen_protos(input_path, output_path, target, lang):
     if(os.path.isdir(input_path)):
@@ -23,23 +23,65 @@ def gen_protos(input_path, output_path, target, lang):
                     print(cmd)
                     print('Success ' + proto)
 
+# Find a file and return the directory
+def find(pattern, path):
+    for root, dirs, files in os.walk(path):
+        for name in files:
+            if fnmatch.fnmatch(name, pattern):
+                return os.path.join(root)
+
+# Get path and make sure the first directory found has all the files in it.
+def check_paths(files_arr):
+    dir = find(files_arr[0], os.getcwd())
+    if (len(files_arr) > 1):
+        for p in range(1, len(files_arr)):
+            current = find(files_arr[p], os.getcwd())
+            if dir == current:
+                return dir + "/"
+            else:
+                print("ERROR: Could not find directory with files: " + str(files_arr))
+                exit(1)
+    else:
+        return dir + "/"
+
+
+# Array that holds all raw protos in SCAII
+# Identifies /SCAII/protos/
+SCAII_protos = ['scaii.proto', 'cfg.proto', 'viz.proto']
+
+# Array that holds compiled names for SCAII
+# Identifies /SCAII/viz/js
+SCAII_vizProtos = ['vizProtos.js']
+
+# Array that holds compiled protos names for SCAII
+# Identifies /SCAII/glue/python/scaii/protos/
+SCAII_python = ['scaii_pb2.py', 'cfg_pb2.py', 'viz_pb2.py']
+
+# Array that holds raw protos names for Sky-RTS 
+# Identifies /Sky-RTS/protos/
+sky_rts_protos = ['sky-rts.proto']
+
+# Array that holds compiled protos names for Sky_RTS
+# Identifies /Sky-RTS/game_wrapper/python/protos/
+sky_rts_python = ['sky_rts_pb2.py']
+
 # Generate vizProtos.js for SCAII
-input_path = '../common_protos/'
-output_path = '../viz/js/'
+input_path = check_paths(SCAII_protos)
+output_path = check_paths(SCAII_vizProtos)
 target = 'vizProtos'
 lang = 'js'
 gen_protos(input_path, output_path, target, lang)
 
 # Generate all python protos for SCAII
-input_path = '../common_protos/'
-output_path = '../glue/python/scaii/protos/'
+input_path = input_path
+output_path = check_paths(SCAII_python)
 target = ''
 lang = 'python'
 gen_protos(input_path, output_path, target, lang)
 
 # Generate all python protos for Sky-rts
-input_path = '../protos/'
-output_path = '../game_wrapper/python/protos/'
+input_path = check_paths(sky_rts_protos)
+output_path = check_paths(sky_rts_python)
 target = ''
 lang = 'python'
 gen_protos(input_path, output_path, target, lang)
